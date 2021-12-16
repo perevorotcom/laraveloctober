e1b12a2e56ccdb300c017da05efd79f7e85e325d
AndreyLanko <lanko@perevorot.com>
2021-12-16 16:56:23 +0200

    Update RedirectTrailingSlash.php

--- src/Perevorotcom/Laraveloctober/Http/Middleware/RedirectTrailingSlash.php
+++ src/Perevorotcom/Laraveloctober/Http/Middleware/RedirectTrailingSlash.php
@@ -11,21 +11,23 @@ class RedirectTrailingSlash
     {
         $url = parse_url($request->getRequestUri());
 
-        if (config('laraveloctober.trailingSlash')) {
-            if (!preg_match('/.+\/$/', $url['path'])) {
-                $url = rtrim($url['path'], '/').'/'.(!empty($url['query']) ? '?'.$url['query'] : '');
+        if ($url['path'] != '/') {
+            if (config('laraveloctober.trailingSlash')) {
+                if (!preg_match('/.+\/$/', $url['path'])) {
+                    $url = rtrim($url['path'], '/').'/'.(!empty($url['query']) ? '?'.$url['query'] : '');
 
-                header('HTTP/1.1 301 Moved Permanently');
-                header('Location: '.$url);
-                exit;
-            }
-        } else {
-            if (preg_match('/.+\/$/', $url['path'])) {
-                $url = rtrim($url['path'], '/').(!empty($url['query']) ? '?'.$url['query'] : '');
+                    header('HTTP/1.1 301 Moved Permanently');
+                    header('Location: '.$url);
+                    exit;
+                }
+            } else {
+                if (preg_match('/.+\/$/', $url['path'])) {
+                    $url = rtrim($url['path'], '/').(!empty($url['query']) ? '?'.$url['query'] : '');
 
-                header('HTTP/1.1 301 Moved Permanently');
-                header('Location: '.$url);
-                exit;
+                    header('HTTP/1.1 301 Moved Permanently');
+                    header('Location: '.$url);
+                    exit;
+                }
             }
         }
 
